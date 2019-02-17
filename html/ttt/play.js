3d3334fe7a7b96f204ee7e62ce08600a2a912a8e
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-02-17 18:58:50 -0500

    2

--- html/ttt/play.js
+++ html/ttt/play.js
@@ -14,8 +14,9 @@ function cellClick(id){
         myRequest.open("POST", "p.php", true);
         myRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         myRequest.send({"grid":board});
-        xhttp.onreadystatechange = function(){
+        myRequest.onreadystatechange = function(){
             if (this.readyState ==4 && this.status ==200){
+                console.log("success");
                 console.log(myRequest.responseText);
             }
         }

a2b0fe73c8e0c8fcec703e116c9e5a836e6b21c6
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-02-17 18:56:36 -0500

    123

--- html/ttt/p.php
+++ html/ttt/p.php
@@ -1,8 +1,8 @@
 <?php
     //header('Access-Control-Allow-Origin:*');//注意！跨域要加这个头 上面那个没有
-    $gird = json_decode($_POST["grid"],true);
-    $gird['8'] = "O";
-    $myjson -> gird = $grid;
+    $grid = $_POST["grid"];
+    $grid['8'] = "O";
+    $myjson -> grid = $grid;
     $myjson -> winner = "me";
     echo json_encode($myjson); 
 ?>

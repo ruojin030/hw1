42dbba8397f17f2fb3a50daf700ff2d666f5c687
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-02-17 16:21:16 -0500

    try1

--- html/ttt/play.js
+++ html/ttt/play.js
@@ -1,1 +1,10 @@
+var winner = document.getElementById("winner");
+function setWinner(winner){
+    winner.insertAdjacentHTML('afterend',winner);
+}
 
+var cell = document.getElementsByClassName("cell");
+cell.addEventListener("click",cellClick(this));
+function cellClick(){
+    this.html("X")
+}

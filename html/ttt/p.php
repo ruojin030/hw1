e0500f57ef14e35a58c42cd68572eff531daacb1
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-02-17 20:33:56 -0500

    try

--- html/ttt/p.php
+++ html/ttt/p.php
@@ -1,9 +1,17 @@
 <?php
     $json= file_get_contents('php://input');
-    $grid = $json["grid"];
-    echo $grid;
-    //$grid['8'] = "O";
-    //$myjson -> grid = $grid;
-    //$myjson -> winner = "me";
-    //echo json_encode($myjson); 
+    //echo $json;
+    //echo $grid;  
+    $grid['0'] = "O";
+    $grid['1'] = " ";
+    $grid['2'] = " ";
+    $grid['3'] = "X";
+    $grid['4'] = "O";
+    $grid['5'] = " ";
+    $grid['6'] = "X";
+    $grid['7'] = " ";
+    $grid['8'] = "O";
+    $myjson -> grid = $grid;
+    $myjson -> winner = "me";
+    echo json_encode($myjson); 
 ?>

d26de767653486f1b22a900e26d7dcaf966c1e57
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-02-17 19:46:35 -0500

    123

--- html/ttt/p.php
+++ html/ttt/p.php
@@ -1,8 +1,9 @@
 <?php
     $json= file_get_contents('php://input');
     $grid = json_decode($json);
-    $grid['8'] = "O";
-    $myjson -> grid = $grid;
-    $myjson -> winner = "me";
-    echo json_encode($myjson); 
+    echo $grid;
+    //$grid['8'] = "O";
+    //$myjson -> grid = $grid;
+    //$myjson -> winner = "me";
+    //echo json_encode($myjson); 
 ?>

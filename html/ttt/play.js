485e3181623c7c4467b826582392b3167984de71
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-02-17 19:08:39 -0500

    123

--- html/ttt/play.js
+++ html/ttt/play.js
@@ -1,4 +1,8 @@
 var board =[' ',' ',' ',' ',' ',' ',' ',' ',' '];
+var grid = {0:" ", 1:" ", 2:" ",
+            3:" ", 4:" ", 5:" ",
+            6:" ", 7:" ", 8:" "
+            };
 var winner = document.getElementById("winner");
 
 function setWinner(winner){
@@ -9,15 +13,18 @@ function cellClick(id){
     if(board[id] == ' '){
         document.getElementById(id).innerHTML="X";
         board[id] = 'X';
+        grid[id] = 'X';
         console.log(board);
         var myRequest = new XMLHttpRequest();
         myRequest.open("POST", "p.php", true);
         myRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        myRequest.send({"grid":board});
+        myRequest.send({"grid":grid});
         myRequest.onreadystatechange = function(){
             if (this.readyState ==4 && this.status ==200){
                 console.log("success");
                 console.log(myRequest.responseText);
+                var result = myRequest.responseText;
+
             }
         }
     }

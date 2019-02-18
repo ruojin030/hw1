388039a5a12672c48dc5bdb772ef45a2725b4925
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-02-17 19:28:55 -0500

    123

--- html/ttt/p.php
+++ html/ttt/p.php
@@ -1,6 +1,6 @@
 <?php
     //header('Access-Control-Allow-Origin:*');//注意！跨域要加这个头 上面那个没有
-    $grid = $_POST["grid"];
+    $grid = $_POST[grid];
     echo $grid;
     //$grid['8'] = "O";
     //$myjson -> grid = $grid;
